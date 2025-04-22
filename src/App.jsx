e22a38cd168423bed653a67e47d0db88cbe20ac9
Dezodev <dezodev@gmail.com>
2025-04-22 22:13:56 +0200

    feat (layout): install mantine and start layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { invoke } from '@tauri-apps/api/core'
-import './App.css'
+import '@mantine/core/styles.css'
+import React, { Children } from 'react'
 
-function App() {
-    const [greetMsg, setGreetMsg] = useState('')
-    const [name, setName] = useState('')
+import { AppShell, Burger, Group, MantineProvider, NavLink, Skeleton } from '@mantine/core'
+import Icon from '@mdi/react'
+import { mdiViewDashboard, mdiCalendar, mdiFormatLetterCase } from '@mdi/js'
 
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        setGreetMsg(await invoke('greet', { name }))
-    }
+const menuItems = [
+    {
+        label: 'Tous les outils',
+        key: 'all-tools',
+        href: '/',
+        icon: <Icon path={mdiViewDashboard} size={1} />,
+    },
+    {
+        label: 'Date',
+        href: '/date',
+        key: 'date',
+        icon: <Icon path={mdiCalendar} size={1} />,
+        childrens: [
+            { label: 'Timestamp', key: 'date-timestamp', href: '/date/timestamp' },
+            { label: 'Opérations', key: 'date-operations', href: '/date/operations' },
+        ],
+    },
+    {
+        label: 'Texte',
+        href: '/text',
+        key: 'text',
+        icon: <Icon path={mdiFormatLetterCase} size={1} />,
+        childrens: [
+            { label: 'Casse', key: 'text-case', href: '/text/case' },
+            { label: 'Base64', key: 'text-base64', href: '/text/base64' },
+        ],
+    },
+]
+
+const MenuNavLink = ({ item, currentKey, ...props }) => {
+    return (
+        <NavLink
+            {...item}
+            active={item.key === currentKey}
+            component="a"
+            leftSection={item.icon ?? null}
+        >
+            {item.childrens?.map((child) => (
+                <MenuNavLink key={child.key} item={child} currentKey={currentKey} {...props} />
+            ))}
+        </NavLink>
+    )
+}
+
+const App = () => {
+    /* State
+     * ================================= */
+    const [opened, setOpened] = React.useState(false)
+    const [currentKey, setCurrentKey] = React.useState('all-tools')
+
+    /* Methods
+     * ================================= */
+
+    const toggle = () => setOpened((o) => !o)
+
+    /* Render
+     * ================================= */
 
     return (
-        <main className="container">
-            <h1>Welcome to Tauri + React</h1>
-
-            <div className="row">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-                </a>
-                <a href="https://tauri.app" target="_blank">
-                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-            <form
-                className="row"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    greet()
-                }}
-            >
-                <input
-                    id="greet-input"
-                    onChange={(e) => setName(e.currentTarget.value)}
-                    placeholder="Enter a name..."
-                />
-                <button type="submit">Greet</button>
-            </form>
-            <p>{greetMsg}</p>
-        </main>
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{ width: 220, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                    <p style={{ marginBottom: 0 }}>DevToolBuddy</p>
+                </Group>
+            </AppShell.Header>
+            <AppShell.Navbar p="md">
+                {menuItems.map((item) => (
+                    <MenuNavLink key={item.key} item={item} currentKey={currentKey} />
+                ))}
+            </AppShell.Navbar>
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
+
+import './assets/css/main.css'
+import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <MantineProvider>
+            <App />
+        </MantineProvider>
     </React.StrictMode>
 )
